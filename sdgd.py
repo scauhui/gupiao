aa1d3dedc35a7be46c4e08958714e4b1fda5f4c1
scauhui <48710948+scauhui@users.noreply.github.com>
2019-07-09 15:59:34 +0800

    Update sdgd.py

--- sdgd.py
+++ sdgd.py
@@ -8,6 +8,9 @@ client = MongoClient()  ##连接数据库
 db = client['hmt']  #指定数据库
 collection = db['sdgd'] #声明对象
 
+L = ['2019-03-31', '2018-12-31', '2018-09-30', '2018-06-30', '2018-03-31', '2017-12-31', '2017-09-30', '2017-06-30', '2017-03-31', '2016-12-31', '2016-09-30', '2016-06-30', '2016-03-31', '2015-09-30', '2015-06-30', '2015-03-31', '2014-12-31', '2014-09-30', '2014-06-30', '2014-03-31', '2013-12-31', '2013-09-30', '2013-06-30', '2013-03-31', '2012-12-31']
+
+
 
 def get_one_page(url):
 	headers = {
@@ -39,17 +42,14 @@ def parse_page(json):
 
 
 if __name__ == '__main__':
-	url = 'http://data.eastmoney.com/DataCenter_V3/gdfx/stockholder.ashx?code=000725&date=2010-06-30&type=Sd'
-	json = get_one_page(url)
-	results = parse_page(json)
-	i = 0
-	for result in results:
-		#print(result)
-		if collection.insert_one(result):
-			i = i + 1
-			print('save to mongo')
-			print(i)
-
-
-
-##时间处要注意url变化（在xhr文件里），还有时间可以写一个规律算法
+	for a in range(0, 26):
+		url = 'http://data.eastmoney.com/DataCenter_V3/gdfx/stockholder.ashx?code=000725&date=' + str(L[a])
+		json = get_one_page(url)
+		results = parse_page(json)
+		i = 0
+		for result in results:
+			#print(result)
+			if collection.insert_one(result):
+				i = i + 1
+				print('save to mongo')
+				print(i)
